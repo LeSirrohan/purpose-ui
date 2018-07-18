0d3ef6e9146407159228f37db4700efb50ddfac4
Hiram Loreto <pjgleixner@msn.com>
2018-07-18 15:22:38 -0500

    Cambios en literales y ajuste en menú de navegación

--- assets/js/feu.js
+++ assets/js/feu.js
@@ -7,13 +7,15 @@ $(document).ready(function($){
     if(ventana_ancho >= 1010){
         $("#btn_min").hide();
         $("a#btn_max").show();
-        /*$("#navbar_main").addClass();
-        $("#navbar_main").removeClass();*/
+        $("#slider1").removeClass("mt-1");
+        $("#slider1").addClass("mt-5");
 
     }
     if(ventana_ancho < 1010){
         $("a#btn_max").hide();
         $("#btn_min").show();
+        $("#slider1").removeClass("mt-5");
+        $("#slider1").addClass("mt-7");
     }
     console.log(ventana_ancho+" "+ventana_alto);
 }); 
