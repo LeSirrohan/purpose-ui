7d26d3f88d9b919cd4ef5dc0584d852f5a318115
Hiram Loreto <pjgleixner@msn.com>
2018-07-18 12:45:40 -0500

    - Formulario de contacto - Ajustes en menú responsive
    Signed-off-by: Hiram Loreto <pjgleixner@msn.com>

--- assets/js/feu.js
+++ assets/js/feu.js
@@ -1,16 +1,19 @@
 $(document).ready(function($){
 	var ventana_ancho = $(window).width();
     var ventana_alto = $(window).height();
-    /*$("#btn_min").hide();
-    $("li#btn_max").show();*/
+    $("a#btn_max").hide();
+    $("#btn_min").show();
 
-    if(ventana_ancho <845){
-        $("#btn_max").hide();
-        $("#btn_min").show();
-    }
-    if(ventana_ancho >=845){
-        $("#btn_max").show();
+    if(ventana_ancho >= 1010){
         $("#btn_min").hide();
+        $("a#btn_max").show();
+        /*$("#navbar_main").addClass();
+        $("#navbar_main").removeClass();*/
+
+    }
+    if(ventana_ancho < 1010){
+        $("a#btn_max").hide();
+        $("#btn_min").show();
     }
     console.log(ventana_ancho+" "+ventana_alto);
 }); 
